ed6da57b91a0bb8318097720d36c3729c30ffd16
ThaBeanBoy <gtchipoyera@gmail.com>
2023-11-11 10:05:19 +0200

    Added a test to check if calls to the console were made

--- tests/dummy.test.ts
+++ tests/dummy.test.ts
@@ -1,19 +1,31 @@
-import { describe, expect, test } from 'vitest';
-import Logger from '../src/creational/singleton';
-import Log from '../src/creational/log';
+import { describe, expect, test, vi, afterAll } from 'vitest';
+import Logger from '../src/creational/singleton/logger';
+
+// https://stackoverflow.com/questions/76042978/in-vitest-how-do-i-assert-that-a-console-log-happened
+const consoleMock = vi
+  .spyOn(console, 'log')
+  .mockImplementation(() => undefined);
+
+const testMessages = ['message 1', 'message 2', 'message 3'];
 
 describe('Logger singleton', () => {
-  test('Add a log', () => {
-    const messages = ['message 1', 'message 2', 'message 3'];
+  afterAll(() => {
+    consoleMock.mockReset();
+  });
 
-    messages.forEach((message) => {
+  test('Add a log', () => {
+    testMessages.forEach((message) => {
       Logger.log(message);
       expect(Logger.logs[Logger.count - 1].message).toBe(message);
     });
   });
 
+  test('Calls to the console made', () => {
+    expect(consoleMock).toBeCalledTimes(3);
+  });
+
   test('Correct count of logs', () => {
     // Added 3 logs in the previous test
-    expect(Logger.count).toBe(3);
+    expect(Logger.count).toBe(testMessages.length);
   });
 });
